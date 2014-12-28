3da48daaad0555a1f4e002201e2906a71c384add
AndrewSB <asbreckenridge@me.com>
2014-12-28 14:20:26 -0800

    reinit without storyboards

--- Ocean.ink Menubar/AppDelegate.swift
+++ Ocean.ink Menubar/AppDelegate.swift
@@ -11,6 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
