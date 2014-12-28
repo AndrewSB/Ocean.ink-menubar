1fba703a8d59c50a1eec559f82ee10cc238f3be8
AndrewSB <asbreckenridge@me.com>
2014-12-28 14:19:15 -0800

    init

---
+++ Ocean.ink Menubar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ocean.ink Menubar
+//
+//  Created by Andrew Breckenridge on 12/28/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
