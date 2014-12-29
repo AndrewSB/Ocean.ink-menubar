b11c779f17ac139fe8e276c688ab5d38edace4f2
AndrewSB <asbreckenridge@me.com>
2014-12-28 16:26:36 -0800

    Revert 7eee6cf..475a9dd
    This rolls back to commit 7eee6cf2437181a96a246224d46cc8cc4b04e750.

--- Ocean.ink Menubar/ContentViewController.swift
+++ Ocean.ink Menubar/ContentViewController.swift
@@ -9,17 +9,10 @@
 import Cocoa
 
 class ContentViewController: NSViewController {
-    let consumerKey = "ImMa1vBc0TLVJxpiknuY1rh8h"
-    let consumerSecret = "s6DzlS4MxacYKHrNYfph3g0xUs7rDAdeeOTGPlFDWCz4cVS71v"
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("Content viewDidLoad")
-        
-        
+        // Do view setup here.
     }
     
-    @IBAction func logInWithTwitterHit(sender: AnyObject) {
-        
-    }
 }
